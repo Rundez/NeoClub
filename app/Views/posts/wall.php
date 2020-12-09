7fc43c2340b861d0a18b6b1698fb47dc4f7e262d
Martin Ruud <m.ruud93@gmail.com>
2020-12-09 10:34:54 +0100

    Added comment input

--- app/Views/posts/wall.php
+++ app/Views/posts/wall.php
@@ -1,10 +1,12 @@
 <div class="container">
-    <h1><?= ($title) ?></h1>
+    <h1 style="margin-top:20px"><?= ($title) ?></h1>
 
+    <?php if(session()->get('isLoggedIn')) : ?>
     <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary float-right" style="margin-bottom: 20px; margin-right: 45%" data-toggle="modal" data-target="#exampleModalCenter">
+    <button type="button" class="btn btn-primary float-right" style="margin-bottom: 20px; margin-right: 30%" data-toggle="modal" data-target="#exampleModalCenter">
         Say something!
     </button>
+    <?php endif; ?>
 
     <?php if (session()->get('error')) : ?>
         <div class="alert alert-danger" role="alert">
@@ -14,10 +16,10 @@
 
     <?php if (!empty($posts) && is_array($posts)) : ?>
         <?php foreach ($posts as $post) : ?>
-            <div class="card mb-3 bg-light shadow-sm" style="width: 55%;">
+            <div class="card bg-light shadow-sm mb-0" style="width: 70%;">
                 <div class="row no-gutters">
                     <div class="col-md-4">
-                    <img src="../uploads/<?= $post['id'] ?>" class="card-img" alt="profile picture">
+                        <img src="../uploads/<?= $post['id'] ?>" class="card-img" alt="profile picture">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
@@ -28,12 +30,30 @@
                     </div>
                 </div>
             </div>
-        <?php endforeach; ?>    
+            <?php if(session()->get('isLoggedIn')) : ?>
+            <!--Input box for commenting -->
+            <div class="card mb-3 bg-light shadow-sm mt-0" style="width: 70%;">
+                <div class="row no-gutters">
+                    <div class="col-md-4" style="text-align: center">
+                    <p><?= session()->get('firstname') . " " . session()->get('lastname') ?></p>
+                    </div>
+                    <div class="col-md-6">
+                        <textarea class="form-control" placeholder="Your comment here" rows="1"></textarea>
+                    </div>
+                    <div class="col-md-2">
+                        <button class="btn btn-primary" style="width: 100%">Comment</button>
+                    </div>
+                </div>
+            </div>
+            <?php else: ?>
+                <div class="mt-3"></div>
+            <?php endif ?>
+        <?php endforeach; ?>
 
 
     <?php else : ?>
 
-        <h3>No News</h3>
+        <h3>No Posts</h3>
 
         <p>Unable to find any posts for you.</p>
 
